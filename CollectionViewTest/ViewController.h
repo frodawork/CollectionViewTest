2274f51a010b2e9cc7af00b713482f9cac00a760
frodawork <frodawork@aliyun.com>
2015-04-29 19:26:23 +0800

    Initial Commit

---
+++ CollectionViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewTest
+//
+//  Created by froda on 15/4/29.
+//  Copyright (c) 2015年 froda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionViewTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CollectionViewTest
+//
+//  Created by froda on 15/4/29.
+//  Copyright (c) 2015年 froda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
